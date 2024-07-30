b724cc16233b3d30c0bf6500806599b8879314a2
Cezuriku <cezuriku@riseup.net>
2024-07-30 13:18:56 +0200

    feat: add banana factories

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,1 +1,61 @@
-fn main() {}
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Factory {
+    production: i32,
+}
+
+fn add_factory(mut commands: Commands) {
+    commands.spawn(Factory { production: 5 });
+}
+
+#[derive(Resource)]
+struct Room {
+    bananas: i32,
+}
+
+#[derive(Resource)]
+struct ProductionTimer(Timer);
+
+fn print_bananas(time: Res<Time>, room: Res<Room>, mut timer: ResMut<ProductionTimer>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        println!("Room bananas: {}", room.bananas)
+    }
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn collect_production(
+    time: Res<Time>,
+    mut room: ResMut<Room>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Factory>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for factory in &query {
+            room.bananas += factory.production
+        }
+    }
+}
+
+pub struct OctoCouscousServerPlugin;
+
+impl Plugin for OctoCouscousServerPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .insert_resource(ProductionTimer(Timer::from_seconds(
+                1.0,
+                TimerMode::Repeating,
+            )))
+            .insert_resource(Room { bananas: 0 })
+            .add_systems(Startup, add_factory)
+            .add_systems(Update, (collect_production, print_bananas));
+    }
+}
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins((MinimalPlugins, OctoCouscousServerPlugin))
+        .run();
+}
