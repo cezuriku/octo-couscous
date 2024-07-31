747250822d0e6a3ca7c21907c77da53d5ce1965f
Cezuriku <cezuriku@riseup.net>
2024-07-31 07:27:36 +0200

    feat: add client server communication

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,59 @@
+use std::net::UdpSocket;
+use std::time::SystemTime;
+
+use bevy::prelude::*;
+use bevy_renet::renet::transport::{ClientAuthentication, NetcodeClientTransport};
+use bevy_renet::renet::{ConnectionConfig, DefaultChannel, RenetClient};
+use bevy_renet::transport::NetcodeClientPlugin;
+use bevy_renet::RenetClientPlugin;
+use rand::Rng;
+
+pub struct OctoCouscousClientPlugin;
+
+impl Plugin for OctoCouscousClientPlugin {
+    fn build(&self, app: &mut App) {
+        let mut rng = rand::thread_rng();
+
+        let client = RenetClient::new(ConnectionConfig::default());
+        app.add_plugins(RenetClientPlugin).insert_resource(client);
+
+        // Setup the transport layer
+        app.add_plugins(NetcodeClientPlugin);
+
+        let authentication = ClientAuthentication::Unsecure {
+            server_addr: "127.0.0.1:5000".parse().unwrap(),
+            client_id: rng.gen(),
+            user_data: None,
+            protocol_id: 0,
+        };
+        let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
+
+        app.insert_resource(transport);
+
+        app.add_systems(Update, (send_message_system, receive_message_system));
+    }
+}
+
 fn main() {
-    println!("Client!");
+    let mut app = App::new();
+    app.add_plugins((MinimalPlugins, OctoCouscousClientPlugin))
+        .run();
+}
+
+// Systems
+
+fn send_message_system(mut client: ResMut<RenetClient>) {
+    // Send a text message to the server
+    // client.send_message(DefaultChannel::ReliableOrdered, "server message");
+}
+
+fn receive_message_system(mut client: ResMut<RenetClient>) {
+    while let Some(message) = client.receive_message(DefaultChannel::ReliableOrdered) {
+        // Handle received message
+        println!("{}", std::str::from_utf8(&message).unwrap())
+    }
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,4 +1,12 @@
+use std::fmt::write;
+use std::net::UdpSocket;
+use std::time::SystemTime;
+
 use bevy::prelude::*;
+use bevy_renet::renet::transport::{NetcodeServerTransport, ServerAuthentication, ServerConfig};
+use bevy_renet::renet::{ConnectionConfig, DefaultChannel, RenetServer, ServerEvent};
+use bevy_renet::transport::NetcodeServerPlugin;
+use bevy_renet::RenetServerPlugin;
 
 #[derive(Component)]
 struct Factory {
@@ -43,14 +51,77 @@ pub struct OctoCouscousServerPlugin;
 
 impl Plugin for OctoCouscousServerPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        let server = RenetServer::new(ConnectionConfig::default());
+
+        let server_addr = "127.0.0.1:5000".parse().unwrap();
+        let socket = UdpSocket::bind(server_addr).unwrap();
+        let server_config = ServerConfig {
+            current_time: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap(),
+            max_clients: 64,
+            protocol_id: 0,
+            public_addresses: vec![server_addr],
+            authentication: ServerAuthentication::Unsecure,
+        };
+        let transport = NetcodeServerTransport::new(server_config, socket).unwrap();
+
+        app.add_plugins(RenetServerPlugin)
+            .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .insert_resource(ProductionTimer(Timer::from_seconds(
                 1.0,
                 TimerMode::Repeating,
             )))
             .insert_resource(Room { bananas: 0 })
             .add_systems(Startup, add_factory)
-            .add_systems(Update, (collect_production, print_bananas));
+            .add_systems(Update, (collect_production, print_bananas))
+            .insert_resource(server)
+            .add_plugins(NetcodeServerPlugin)
+            .insert_resource(transport)
+            .add_systems(
+                Update,
+                (
+                    send_message_system,
+                    receive_message_system,
+                    handle_events_system,
+                ),
+            );
+    }
+}
+
+fn send_message_system(mut server: ResMut<RenetServer>, room: Res<Room>) {
+    if room.is_changed() {
+        for client_id in server.clients_id() {
+            let message = format!("Bananas: {}", room.bananas);
+            server.send_message(client_id, DefaultChannel::ReliableOrdered, message)
+        }
+    }
+}
+
+fn receive_message_system(mut server: ResMut<RenetServer>) {
+    // Receive message from all clients
+    for client_id in server.clients_id() {
+        while let Some(message) = server.receive_message(client_id, DefaultChannel::ReliableOrdered)
+        {
+            println!(
+                "Message from id:{client_id} {}",
+                std::str::from_utf8(&message).unwrap()
+            )
+            // Handle received message
+        }
+    }
+}
+
+fn handle_events_system(mut server_events: EventReader<ServerEvent>) {
+    for event in server_events.read() {
+        match event {
+            ServerEvent::ClientConnected { client_id } => {
+                println!("Client {client_id} connected");
+            }
+            ServerEvent::ClientDisconnected { client_id, reason } => {
+                println!("Client {client_id} disconnected: {reason}");
+            }
+        }
     }
 }
 
