f49501fead76e1f307bea1cff75ab94675d24670
Cezuriku <cezuriku@riseup.net>
2024-08-16 09:02:13 +0200

    test: move system test to systems folder

--- map_plugin/src/lib.rs
+++ map_plugin/src/lib.rs
@@ -17,11 +17,6 @@ impl Plugin for MapPlugin {
 
 #[cfg(test)]
 mod tests {
-    use std::time::Duration;
-
-    use bevy::time::{TimePlugin, TimeUpdateStrategy};
-    use components::GoldMine;
-    use resources::Map;
 
     use super::*;
 
@@ -29,51 +24,5 @@ mod tests {
     fn can_create_factory() {
         let factory = components::GoldMine { production: 5 };
         assert_eq!(factory.production, 5)
-    }
-
-    #[test]
-    fn gold_mine_generate_gold() {
-        let mut app = App::new();
-        let gold_production = 3;
-        app.add_plugins(TimePlugin)
-            .add_systems(Startup, create_map)
-            .add_systems(Update, collect_gold)
-            .insert_resource(TimeUpdateStrategy::ManualDuration(Duration::from_millis(
-                100,
-            )));
-
-        // Setup test entities
-        app.world_mut().spawn(GoldMine {
-            production: gold_production,
-        });
-
-        // From 0 seconds to 0.5 we should have 0 gold.
-        for _ in 0..5 {
-            app.update();
-            let map = app.world().resource::<Map>();
-            assert_eq!(map.gold, 0, "No gold should be present at the beginning");
-        }
-
-        // After 0.5 seconds we should have 1 production
-        for _ in 0..5 {
-            app.update();
-            let map = app.world().resource::<Map>();
-            assert_eq!(
-                map.gold, gold_production,
-                "The first production should have produced {gold_production} gold"
-            );
-        }
-
-        // After 0.5 seconds we should have 2 production
-        for _ in 0..5 {
-            app.update();
-            let map = app.world().resource::<Map>();
-            assert_eq!(
-                map.gold,
-                2 * gold_production,
-                "We should now have {} gold",
-                2 * gold_production
-            );
-        }
     }
 }
--- map_plugin/src/systems/mod.rs
+++ map_plugin/src/systems/mod.rs
@@ -28,3 +28,74 @@ pub fn collect_gold(
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use crate::components::GoldMine;
+    use crate::resources::Map;
+    use bevy::time::{TimePlugin, TimeUpdateStrategy};
+
+    use super::*;
+
+    #[test]
+    fn gold_mine_generate_gold() {
+        let mut app = App::new();
+        let initial_gold = 13;
+        let gold_production = 5;
+        app.add_plugins(TimePlugin)
+            .add_systems(Update, collect_gold)
+            .insert_resource(TimeUpdateStrategy::ManualDuration(Duration::from_millis(
+                100,
+            )));
+
+        // Setup test entities
+        app.world_mut().insert_resource(Map {
+            gold: initial_gold,
+            width: 5,
+            height: 5,
+        });
+        app.world_mut()
+            .insert_resource(GoldTimer(Timer::from_seconds(0.5, TimerMode::Repeating)));
+        app.world_mut().spawn(GoldMine {
+            production: gold_production,
+        });
+
+        // Between 0 and 0.5 seconds we should have only initial_gold
+        for _ in 0..5 {
+            app.update();
+            let map = app.world().resource::<Map>();
+            assert_eq!(
+                map.gold, initial_gold,
+                "We should now have {} gold",
+                initial_gold
+            );
+        }
+
+        // Between 0.5 and 1 second we should have harvested the first gold production
+        for _ in 0..5 {
+            app.update();
+            let map = app.world().resource::<Map>();
+            assert_eq!(
+                map.gold,
+                initial_gold + gold_production,
+                "We should now have {} gold",
+                initial_gold + gold_production
+            );
+        }
+
+        // Between 0.5 and 1 second we should have harvested
+        // the first and second gold productions
+        for _ in 0..5 {
+            app.update();
+            let map = app.world().resource::<Map>();
+            assert_eq!(
+                map.gold,
+                initial_gold + 2 * gold_production,
+                "We should now have {} gold",
+                initial_gold + 2 * gold_production
+            );
+        }
+    }
+}
