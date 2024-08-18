3aded7c634b476d16bb01361aa6098d731f57e0c
Cezuriku <cezuriku@riseup.net>
2024-08-18 11:51:16 +0200

    feat: add godot lib

---
+++ godot_lib/src/lib.rs
@@ -0,0 +1,75 @@
+use std::net::SocketAddr;
+use std::net::UdpSocket;
+use std::time::Duration;
+use std::time::SystemTime;
+
+use godot::classes::*;
+use godot::prelude::*;
+use renet::transport::ClientAuthentication;
+use renet::transport::NetcodeClientTransport;
+use renet::ConnectionConfig;
+use renet::RenetClient;
+
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
+
+#[derive(GodotClass)]
+#[class(base=Control)]
+struct NetworkControl {
+    base: Base<Control>,
+    server_addr: Option<SocketAddr>,
+    client: RenetClient,
+    transport: Option<NetcodeClientTransport>,
+}
+
+#[godot_api]
+impl IControl for NetworkControl {
+    fn init(base: Base<Control>) -> Self {
+        godot_print!("Network init");
+
+        Self {
+            base,
+            server_addr: None, // If None, connection was not started
+            client: RenetClient::new(ConnectionConfig::default()),
+            transport: None,
+        }
+    }
+
+    fn process(&mut self, delta: f64) {
+        if let Some(transport) = self.transport.as_mut() {
+            self.client.update(Duration::from_secs_f64(delta));
+            transport
+                .update(Duration::from_secs_f64(delta), &mut self.client)
+                .unwrap();
+
+            transport.send_packets(&mut self.client).unwrap();
+        }
+    }
+}
+
+#[godot_api]
+impl NetworkControl {
+    #[func]
+    pub fn connect_to_host(&mut self, host: GString) {
+        godot_print!("Connect to host");
+
+        self.server_addr = Some(SocketAddr::V4(host.to_string().parse().unwrap()));
+        let addr = self.server_addr.unwrap();
+
+        let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let authentication = ClientAuthentication::Unsecure {
+            server_addr: addr,
+            client_id: 0,
+            user_data: None,
+            protocol_id: 0,
+        };
+
+        self.transport =
+            Some(NetcodeClientTransport::new(current_time, authentication, socket).unwrap());
+    }
+}
